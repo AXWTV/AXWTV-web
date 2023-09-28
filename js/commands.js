36840946077f0042fc90273d4397a9440d07832f
AXWTV <azmainsharer@gmail.com>
2023-09-28 21:20:35 +0600

    first commit

---
+++ js/commands.js
@@ -0,0 +1,107 @@
+// Deffenetly by AXWTV
+// Credit to ForrestKnight
+var youtube = "https://www.youtube.com/AXWTV/";
+var twitter = "https://www.twitter.com/AXWTV/";
+var password = "AXWTV";
+//var linkedin = "https://www.linkedin.com/*";
+var instagram = "https://www.instagram.com/_.a.x.w.t.v/";
+var github = "https://github.com/AXWTV/";
+var email = 'azmainxwtv@gmail.com';
+
+whois = [
+  "<br>",
+  "Welcome to my website I'm AXWTV, and I'm thrilled to have you here.",
+  "Allow me to introduce myself and share a little bit about what makes me tick.",
+  "First and foremost, I'm a passionate anime aficionado.",
+  "The vivid worlds, compelling characters, and imaginative storytelling of anime have always captured my heart.",
+  "Whether it's diving into classic series or exploring the latest releases, my love for anime is an integral part of who I am.",
+  "Beyond my anime obsession, I'm a self-made programmer.",
+  "I've delved deep into the world of coding, crafting lines of logic and algorithms to bring my digital dreams to life",
+  "Well that part is a Lie!",
+  "But The Rest is True!",
+  "One of my favorite ways to combine my programming skills with my love for anime is by creating anime-themed projects.",
+  "Whether it's designing a website dedicated to my favorite series, developing apps that enhance the anime-watching experience,",
+  "or even crafting unique art inspired by the anime I adore, I'm always looking for ways to fuse technology and creativity.",
+  "You can also find me on YouTube, where I share my thoughts, reviews, and analyses of anime and technology-related topics.",
+  "It's a space where I get to connect with fellow anime enthusiasts and tech aficionados,",
+  "diving deep into discussions and sharing my perspective on the things that matter to me.",
+  "While my YouTube channel keeps me busy, I'm excited to share that I have big plans for the future.",
+  "I'm gearing up to start my own podcast, where I'll explore a wide range of subjects, from anime and programming to pop culture and beyond.",
+  "Stay tuned for insightful conversations, guest interviews, and thought-provoking discussions.",
+  "Thank you for joining me on this journey of anime, programming, and creative expression.",
+  "I'm thrilled to have you as part of my community, and I can't wait to share more with you in the days to come.",
+  "Feel free to explore my website, engage with my content, and let's embark on this exciting adventure together!",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "Knowing yourself is the beginning of all wisdom." ,
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/AXWTV' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/AXWTV' + '</a>',
+//  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/AXWTV' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/AXWTV' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/AXWTV' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Hmm...I think they are on my Github",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is AXWTV?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         MY Social Stuff',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+ls = [
+  "<br>",
+  " .   ..",
+  "<br>",
+];
+
+banner = [
+  '<span class="index">AXWTV Not A Corporation. All knights reserved.</span>',
+  "⠀⠀⠀⠀⠀⠀⠀⢀⠀⠔⡀⠀⢀⠞⢰⠂⠀⠀⠀⠀⠀⠀⠀",
+  "⠀⠀⠀⠀⠀⠀⠀⢸⠘⢰⡃⠔⠩⠤⠦⠤⢀⡀⠀⠀⠀⠀⠀",
+  "⠀⠀⠀⢀⠄⢒⠒⠺⠆⠈⠀⠀⢐⣂⠤⠄⡀⠯⠕⣒⣒⡀⠀",
+  "⠀⠀⢐⡡⠔⠁⠆⠀⠀⠀⠀⠀⢀⠠⠙⢆⠀⠈⢁⠋⠥⣀⣀",
+  "⠈⠉⠀⣰⠀⠀⠀⠀⡀⠀⢰⣆⢠⠠⢡⡀⢂⣗⣖⢝⡎⠉⠀",
+  "⢠⡴⠛⡇⠀⠐⠀⡄⣡⢇⠸⢸⢸⡇⠂⡝⠌⢷⢫⢮⡜⡀⠀",
+  "⠀⠀⢰⣜⠘⡀⢡⠰⠳⣎⢂⣟⡎⠘⣬⡕⣈⣼⠢⠹⡟⠇⠀",
+  "⠀⠠⢋⢿⢳⢼⣄⣆⣦⣱⣿⣿⣿⣷⠬⣿⣿⣿⣿⠑⠵⠀⠀",
+  "⠀⠀⠀⡜⢩⣯⢝⡀⠁⠀⠙⠛⠛⠃⠀⠈⠛⠛⡿⠀⠀⠀⠀",
+  "⠀⠀⠀⠀⠀⣿⠢⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀",
+  "⠀⠀⠀⠀⢀⣀⡇⠀⠑⠀⠀⠀⠀⠐⢄⠄⢀⡼⠃⠀⠀⠀⠀",
+  "⠀⠀⠀⠀⢸⣿⣷⣤⣀⠈⠲⡤⣀⣀⠀⡰⠋⠀⠀⠀⠀⠀⠀",
+  "⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣶⣤⣙⣷⣅⡀⠀⠀⠀⠀⠀⠀▄▀▄ ▀▄▀ █ █ █ ▀█▀ █ █ ⠀",
+  "⠀⠀⢀⣾⣿⣿⣿⣿⣻⢿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀█▀█ █ █ ▀▄▀▄▀  █  ▀▄▀  ",
+  "⠀⡠⠟⠁⠙⠟⠛⠛⢿⣿⣾⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀",
+  "                            ",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
